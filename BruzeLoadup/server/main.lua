3be5bb58efcbb6eda177783790b2c8c65529bcc5
byBlurr <dbldumont11@gmail.com>
2018-04-06 16:09:30 +0100

    BruzeLoadup
    - Removed the resource

--- BruzeLoadup/server/main.lua
+++
@@ -1,5 +0,0 @@
---[[ Kick a player if they try joining ]]--
-AddEventHandler('playerConnecting', function(playerName, setKickReason)
-    setKickReason("Connection Refused:Server on startup")
-    CancelEvent()
-end)
